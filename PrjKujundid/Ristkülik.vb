89145a5c730aceef95ed332822009f5f98b153b1
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-28 13:34:27 +0200

    on lisatud ja tehtud klass ristkülik

---
+++ PrjKujundid/Ristkülik.vb
@@ -0,0 +1,14 @@
+﻿Public Class Ristkülik
+    Inherits Nelinurk
+
+    'Konstruktor baasklassi atribuutide väärtustamiseks
+    Public Sub New(ByVal alus As Double, ByVal korgus As Double)
+        'Baasklassi konstruktori väljakutse
+        MyBase.New("Ristkülik", alus, korgus, alus, korgus)
+    End Sub
+
+    'Funktsioon arvutab ja tagastab ruudu pindala
+    Public Overrides Function leiaPindala() As Double
+        Return kylgA * kylgB
+    End Function
+End Class
