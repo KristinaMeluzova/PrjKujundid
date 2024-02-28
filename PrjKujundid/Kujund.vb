de37318adeb1e750da8957d605de9373373996ad
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-28 12:55:02 +0200

    On lisatud klass Kujund

---
+++ PrjKujundid/Kujund.vb
@@ -0,0 +1,9 @@
+﻿Public Class Kujund
+    'Abstraktne baasklass kujundite jaoks
+    Public MustInherit Class Kujund
+        Private tyyp As String
+        Protected kulgA As Double
+        Protected kulgB As Double
+        Protected kulgC As Double
+    End Class
+End Class
