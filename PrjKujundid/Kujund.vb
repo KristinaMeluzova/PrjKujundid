7066beceb093bd2b158008dd72323d3bea402405
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-28 13:05:40 +0200

    On muudatud ja lõpetatud baasklass Kujund

--- PrjKujundid/Kujund.vb
+++ PrjKujundid/Kujund.vb
@@ -1,9 +1,28 @@
-﻿Public Class Kujund
-    'Abstraktne baasklass kujundite jaoks
-    Public MustInherit Class Kujund
-        Private tyyp As String
-        Protected kulgA As Double
-        Protected kulgB As Double
-        Protected kulgC As Double
-    End Class
+﻿'Abstraktne baasklass kujundite jaoks
+Public MustInherit Class Kujund
+    Private tyyp As String
+    Protected kylgA As Double
+    Protected kylgB As Double
+    Protected kylgC As Double
+
+    'Konstruktor atribuutide algväärtustamiseks
+    'Atribuudid: kylg_a, kylg_b, kylg_c
+    Public Sub New(ByVal tyyp As String, ByVal kylgA As Double,
+                   ByVal kylgB As Double, ByVal kylgC As Double)
+        Me.tyyp = tyyp
+        Me.kylgA = kylgA
+        Me.kylgB = kylgB
+        Me.kylgC = kylgC
+    End Sub
+
+    'Funktsioon tagastab kujundi tüübi
+    Public Function annaTyyp() As String
+        Return tyyp
+    End Function
+
+    'Abstraktne funktsioon kujundi ümbernõõdu arvutamiseks
+    Public MustOverride Function leiaYmberMoot() As Double
+
+    'Abstraktne funktsioon kujundi ümbernõõdu arvutamiseks
+    Public MustOverride Function leiaPindala() As Double
 End Class
