9c9bd386e9c457b183458133c5042947284fe346
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-28 13:15:29 +0200

    On lisatud ja tehtud Kolmnurk klass

---
+++ PrjKujundid/Kolmnurk.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Math
+Public Class Kolmnurk
+    Inherits Kujund
+
+    Private korgus As Double
+
+    'Konstruktor baasklassi atribuutide väärtustamiseks
+    Public Sub New(ByVal alus As Double, ByVal haar As Double)
+        'Kutsume välja baasklassi konstruktori
+        MyBase.New("Võrdhaarne kolmnurk", alus, haar, haar)
+
+        'Kolmnurga kõrgus
+        Me.korgus = Sqrt(haar * haar - (alus * alus / 4))
+    End Sub
+    Public Overrides Function leiaYmberMoot() As Double
+        Return kylgA * korgus / 2
+    End Function
+
+    Public Overrides Function leiaPindala() As Double
+        Return kylgA + kylgB + kylgC
+    End Function
+End Class
